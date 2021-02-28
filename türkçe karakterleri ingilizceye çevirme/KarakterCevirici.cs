550220208bf5f5e75ad9a52f6afbf647034881e1
Hüseyin DEMİRDÖĞER <huseyindemirdoger1992@hotmail.com>
2021-03-01 02:04:22 +0300

    .

---
+++ türkçe karakterleri ingilizceye çevirme/KarakterCevirici.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace türkçe_karakterleri_ingilizceye_çevirme
+{
+    public class KarakterCevirici
+    {
+        public static string karakterCevir(string kelime)
+        {
+            string mesaj = kelime;
+            char[] oldValue = new char[] { 'ö', 'Ö', 'ü', 'Ü', 'ç', 'Ç', 'İ', 'ı', 'Ğ', 'ğ', 'Ş', 'ş' ,' ' };
+            char[] newValue = new char[] { 'o', 'O', 'u', 'U', 'c', 'C', 'I', 'i', 'G', 'g', 'S', 's' ,'_'};
+            for (int sayac = 0; sayac < oldValue.Length; sayac++)
+            {
+                mesaj = mesaj.Replace(oldValue[sayac], newValue[sayac]);
+            }
+            return mesaj;
+        }
+    }
+}
